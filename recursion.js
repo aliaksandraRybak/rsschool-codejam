2eed1a8ab70fde5a357f23e2b39ec210e11935f2
aliaksandraRybak <aliaksandra.rybak@gmail.com>
2018-10-28 19:32:48 +0300

    refactor: write code according to eslint

--- recursion.js
+++ recursion.js
@@ -1,40 +1,35 @@
-function recursion(tree) {
-	const map = new Map();
-	let level = 1;
+module.exports = function recursion(tree) {
+  const map = new Map();
+  const level = 1;
 
-	function setLevels(level, newTree) {
-		if (map.has(level)) {
-			map.get(level).push(newTree.value);
-		} else {
-			map.set(level, [newTree.value]);
-		}
+  function goDeep(lvl, newTree) {
+    let thisLevel = lvl;
+    if (map.has(thisLevel)) {
+      map.get(thisLevel).push(newTree.value);
+    } else {
+      map.set(thisLevel, [newTree.value]);
+    }
 
-		level++;
-		if (newTree.left) {
-			setLevels(level, newTree.left);
-		}
+    thisLevel += 1;
+    if (newTree.left) {
+      goDeep(thisLevel, newTree.left);
+    }
 
-		if (newTree.right) {
-			setLevels(level, newTree.right);
-		}
-	}
+    if (newTree.right) {
+      goDeep(thisLevel, newTree.right);
+    }
+  }
 
-	setLevels(level, tree);
+  goDeep(level, tree);
 
-	const arrays = map.values();
-	const res = [];
+  const arrays = map.values();
+  const res = [];
 
-	let arr = arrays.next().value;
-	while (arr) {
-		res.push(arr);
-		arr = arrays.next().value;
-	}
+  let arr = arrays.next().value;
+  while (arr) {
+    res.push(arr);
+    arr = arrays.next().value;
+  }
 
-	return res;
-}
-
-
-let tree = {"value":100,"left":{"value":90,"left":{"value":70},"right":{"value":99}},"right":{"value":120,"left":{"value":110},"right":{"value":130}}};
-let array = recursion(tree);
-
-console.log(array); // [[100], [90, 120], [70,99,110,130]]
+  return res;
+};
